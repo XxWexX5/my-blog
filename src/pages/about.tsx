4822e1ea328f2c0f1d2b61b77510eb089e41385a
XxWexX5 <37388037+XxWexX5@users.noreply.github.com>
2022-04-23 17:37:55 -0300

    [ FEATURE ] Using useStaticQuery.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,57 +1,56 @@
 import React from "react"
 
-import { Link, StaticQuery, graphql } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const AboutPage = () => (
-  <Layout>
-    <Seo title="About" />
-
-    <h1>About</h1>
-
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-
-      <li>
-        <Link to="/about" activeStyle={{ color: "#B22B27" }}>
-          About
-        </Link>
-      </li>
-    </ul>
-
-    <StaticQuery
-      query={graphql`
-        {
-          site {
-            siteMetadata {
-              title
-              description
-              author
-              siteUrl
-            }
-          }
+const AboutPage = () => {
+  const {
+    site: {
+      siteMetadata: { title, description, author, siteUrl },
+    },
+  } = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          title
+          description
+          author
+          siteUrl
         }
-      `}
-      render={({
-        site: {
-          siteMetadata: { title, description, author, siteUrl },
-        },
-      }) => (
-        <div className="wrapper-info-page">
-          <h2>{title}</h2>
-          <p>{description}</p>
-          <p>{author}</p>
-          <Link to={siteUrl} target="_blank">
-            {siteUrl}
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <Seo title="About" />
+
+      <h1>About</h1>
+
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/about" activeStyle={{ color: "#B22B27" }}>
+            About
           </Link>
-        </div>
-      )}
-    />
-  </Layout>
-)
+        </li>
+      </ul>
+
+      <div className="wrapper-info-page">
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <p>{author}</p>
+        <Link to={siteUrl} target="_blank">
+          {siteUrl}
+        </Link>
+      </div>
+    </Layout>
+  )
+}
 
 export default AboutPage
