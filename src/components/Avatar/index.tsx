eade9bfbfd50d90c2a399477a2b10d70c6660aae
XxWexX5 <37388037+XxWexX5@users.noreply.github.com>
2022-04-24 16:34:10 -0300

    [ FEATURE ] Adding image with gatsby.

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const Avatar = () => {
+  const { avatarImage } = useStaticQuery(
+    graphql`
+      query {
+        avatarImage: file(relativePath: { eq: "image-profile.jpeg" }) {
+          childImageSharp {
+            fixed(width: 125, height: 125) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return <Img fixed={avatarImage.childImageSharp.fixed} />
+}
+
+export default Avatar
--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,7 +1,11 @@
 import React from "react"
 
+import Avatar from "../Avatar"
+
 const Profile = () => (
   <div className="wrapper-profile">
+    <Avatar />
+
     <h2 className="title">Wevison</h2>
     <h3 className="subtitle">Front-end Developer</h3>
     <p className="descript">A great developer.</p>
